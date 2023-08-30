e3bce57d3a13302e85ad814c054be8f2e77bda5f
amitpandey13 <aspp9308@gmail.com>
2023-08-30 14:17:13 +0530

    wrong commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	public static Logger logge = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		logger.info("main class started...");
+
+
+		logger.info("mai kru to sala character dheela hai..");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
